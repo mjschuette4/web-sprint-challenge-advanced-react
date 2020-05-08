803acbd981c66e25266ea4ec0bad05cdd156fa6f
MatthewSchuette <mjschuette4@gmail.com>
2020-05-08 13:53:02 -0500

    cleaned up test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -15,21 +15,10 @@ test("form shows success message on submit with form details", () => {
 
     const submit = getByTestId("submit");
     const firstName = getByLabelText(/First Name/i);
-    // const lastName = getByText(/Last Name/i);
-    // const address = getByText(/Address/i);
-    // const city = getByText(/City/i);
-    // const state = getByText(/State/i);
-    // const zip = getByText(/Zip/i);
 
     fireEvent.change(firstName, {target: {value:"Matthew"}});
-    // fireEvent.change(lastName, {target: {value:"Schuette"}});
-    // fireEvent.change(address, {target: {value:"109 Santa Cruz Ct."}})
-    // fireEvent.change(city, {target: {value:"Slidell"}})
-    // fireEvent.change(state, {target: {value:"Louisiana"}});
-    // fireEvent.change(zip, {target: {value:"70458"}});
     fireEvent.click(submit);
 
     findAllByText(/Matthew/i);
-
     expect(getByTestId("successMessage")).toBeInTheDocument();  
 });
